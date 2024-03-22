b6bfbdc7bde0ef89a289bbfc7945c5750b786fc7
Ivan Merkulov <ivan.merkulov@student.gyarab.cz>
2024-03-22 11:24:33 +0100

    jasne poke

--- gaflix/urls.py
+++ gaflix/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='filmy/base.html'))
+    path('', TemplateView.as_view(template_name='filmy/home.html')),
+    path('druhy/', TemplateView.as_view(template_name='filmy/druhy.html')),
 ]
